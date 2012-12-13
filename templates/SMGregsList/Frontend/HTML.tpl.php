55ed09e233e72eb1324b975bc20d773dee645eca
Gregory Beaver <greg@chiaraquartet.net>
2012-12-13 16:50:17 -0600

    make controls slightly smaller

--- templates/SMGregsList/Frontend/HTML.tpl.php
+++ templates/SMGregsList/Frontend/HTML.tpl.php
@@ -3,6 +3,17 @@
  <head>
   <title>Greg's List - Striker Manager transfer market <?php echo $context->subtitle ?></title>
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+   <style media="screen" type="text/css">
+    .form-horizontal .control-group {
+      margin-bottom: 1px;
+    }
+    input[type="text"] {
+     height: 15px;
+    }
+    select {
+     height: 25px;
+    }
+   </style>
  </head>
  <body>
   <div class="row-fluid">
