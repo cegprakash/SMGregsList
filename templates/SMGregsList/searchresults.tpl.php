a6d20e3b3fd6850ad8517484c24ad15bcf721aff
Gregory Beaver <greg@chiaraquartet.net>
2012-12-13 22:17:02 -0600

    try to fix this odd bug.  Copy database from live server to help debug this issue

--- templates/SMGregsList/searchresults.tpl.php
+++ templates/SMGregsList/searchresults.tpl.php
@@ -1,4 +1,5 @@
     <?php if ($context->getRawObject() instanceof SMGregsList\Player) {
         echo $savant->render(array($context));
-    }
-    echo $savant->render($context->getRawObject()->getArrayCopy()); ?>
+    } else {
+        echo $savant->render($context->getRawObject()->getArrayCopy());
+    }?>
