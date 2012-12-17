568d50cc32b17bb586d45a208c930696566ed609
Gregory Beaver <greg@chiaraquartet.net>
2012-12-16 22:48:28 -0600

    add frontend for json input

---
+++ jsonrpc.php
@@ -0,0 +1,9 @@
+<?php
+namespace SMGregsList;
+include __DIR__ . '/autoload.php';
+
+Backend\DataLayer\Sqlite3::$DATABASEPATH = __DIR__ . '/data/test.db';
+// basic web frontend, chrome extension frontend is json.php
+$frontend = new Frontend\Json;
+$main = new Main($frontend);
+$main->start();
