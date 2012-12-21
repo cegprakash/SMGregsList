7232588080c6d9fe6404a5a67d6203246e2f7e32
Gregory Beaver <greg@chiaraquartet.net>
2012-12-20 18:07:58 -0600

    add a link to the search in the market page of SM

---
+++ chromeext/parsemarket.js
@@ -0,0 +1,13 @@
+(function() {
+  var market = document.getElementsByClassName('botonesright');
+  for (var i = 0;i < market.length; i++) {
+    if (market[i].firstChild.nextSibling.className == "botoncorto") {
+      var menuitem = document.createElement('a');
+      menuitem.className = 'botoncorto';
+      menuitem.href = 'http://chiaraquartet.net/sm/index.php';
+      menuitem.appendChild(document.createTextNode("Transfer Market"));
+      market[i].insertBefore(menuitem, market[i].firstChild.nextSibling);
+      return;
+    }
+  }
+})();
