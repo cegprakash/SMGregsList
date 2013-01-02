78eacbfb6e8503d701ae8f7200208db4c4d47a20
Gregory Beaver <greg@chiaraquartet.net>
2013-01-02 14:33:48 -0600

    add manager/code if we have it

--- chromeext/parsesquad.js
+++ chromeext/parsesquad.js
@@ -9,38 +9,49 @@ function scrapepage() {
     var player = players[i].match(/jugador.php\?id_jugador=([0-9]+)/);
     ids.push(Number(player[1]));
   }
-  remote("exists", {'ids': ids}, function(result) {
-    if (result.error) {
-      if (sm_debug) {
-        alert(result.error.message);
-      } else {
-        console.log(result.error.message);
+  chrome.extension.sendMessage([6], function(response) {
+    chrome.storage.sync.get(['SMGregsList.codes'], function(a) {
+      var codes = a['SMGregsList.codes'];
+      var parentuser = parenthtml.match(/<a target="marco" href="usuario.php" class="color_skin" ?>([^<]+)<\/a/);
+      var params = {'ids': ids};
+      if (codes[parentuser]) {
+        params.manager = parentuser;
+        params.code = codes[parentuser];
       }
-      return;
-    }
-    for (var b = 0; b < ['1','2'].length; b++) {
-      var j = ['1', '2'][b];
-      var kiddos = document.getElementsByClassName('tipo' + j);
-      for (var i = 0; i < kiddos.length; i++) {
-        // I love DOM...
-        var kiddo = kiddos[i].firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
-          .nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling;
-        var href = kiddos[i].firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.href;
-        var idcheck = href.match(/jugador.php\?id_jugador=([0-9]+)/);
-        if (result.params.exists[Number(idcheck[1])]) {
-          // player is for sale
-          var forsale = document.createElement('img');
-          var loc = chrome.i18n.getMessage("@@extension_id");
-          forsale.src = 'chrome-extension://' + loc + '/icon16.png';
-          forsale.style.width = "16px";
-          forsale.style.height = "15px";
-          forsale.title = 'Player is for sale by Transfer Agreement';
-          forsale.style.width = "16px";
-          forsale.style.height = "15px;";
-          kiddo.appendChild(forsale);
+      remote("exists", params, function(result) {
+        if (result.error) {
+          if (sm_debug) {
+            alert(result.error.message);
+          } else {
+            console.log(result.error.message);
+          }
+          return;
         }
-      }
-    }
+        for (var b = 0; b < ['1','2'].length; b++) {
+          var j = ['1', '2'][b];
+          var kiddos = document.getElementsByClassName('tipo' + j);
+          for (var i = 0; i < kiddos.length; i++) {
+            // I love DOM...
+            var kiddo = kiddos[i].firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
+              .nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling;
+            var href = kiddos[i].firstChild.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.firstChild.href;
+            var idcheck = href.match(/jugador.php\?id_jugador=([0-9]+)/);
+            if (result.params.exists[Number(idcheck[1])]) {
+              // player is for sale
+              var forsale = document.createElement('img');
+              var loc = chrome.i18n.getMessage("@@extension_id");
+              forsale.src = 'chrome-extension://' + loc + '/icon16.png';
+              forsale.style.width = "16px";
+              forsale.style.height = "15px";
+              forsale.title = 'Player is for sale by Transfer Agreement';
+              forsale.style.width = "16px";
+              forsale.style.height = "15px;";
+              kiddo.appendChild(forsale);
+            }
+          }
+        }
+      });
+    });
   });
 }
 scrapepage();
